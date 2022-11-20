50cf6c77cccb21c3377507583d7394a4e89d77e3
--global <--global>
2022-11-20 19:08:04 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+echo "Hello World!<br>";
+
+$nama = "Nurul";
+$umur = "21";
+
+echo "Nama saya <strong> $nama </strong>, saya berumur $umur tahun.<br>";
+
+$namaAdik = "Ruka";
+$umurAdik = "19";
+
+echo "Nama adik saya <strong> $namaAdik </strong>, adik saya berumur $umurAdik tahun.<br>";
+
+$selisihUmur = $umur - $umurAdik;
+
+echo "Selisih usia saya dan adik saya adalah $selisihUmur tahun."
+
+
+
+?>
